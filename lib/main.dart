89f6e05046ae2aab2a6858080e2a0927b95a6093
Arkaraj <arkaraj2017@gmail.com>
2021-06-19 23:57:47 +0530

    Playing around with flutter

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,55 @@ import 'package:flutter/material.dart';
 
 // Widget nesting stuff, default material UI font and themes
 void main() => runApp(MaterialApp(
-  home: memes(),
-));
+      home: memes(),
+    ));
 
 // Stateless Widget - State of the widget will not change overtime
 class memes extends StatelessWidget {
+  final ButtonStyle textButtonStyle = TextButton.styleFrom(
+    primary: Colors.white,
+    minimumSize: Size(88, 44),
+    padding: EdgeInsets.symmetric(horizontal: 16.0),
+    shape: const RoundedRectangleBorder(
+      borderRadius: BorderRadius.all(Radius.circular(2.0)),
+    ),
+    backgroundColor: Colors.amber,
+  );
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-    appBar: AppBar(
-      title: Text('Flutter Demo'),
-      centerTitle: true,
-      backgroundColor: Colors.blue[400],
-    ),
-    body: Center(
-      child: Text(
-        'Meme API App',
-        style: TextStyle(
-          fontSize: 20,
-          fontFamily: 'Karla',
-          fontWeight: FontWeight.bold,
-          letterSpacing: 2,
-          color: Colors.grey[600],
-        ),
+      appBar: AppBar(
+        title: Text('Flutter Demo'),
+        centerTitle: true,
+        backgroundColor: Colors.blue[400],
       ),
-    ),
-    floatingActionButton: FloatingActionButton(
-      child: Icon(Icons.bolt), // Text("Click me")
-      onPressed: () => print("clicked"),
-      backgroundColor: Colors.blue[400],
-    ),
-  );
+      body: Column(
+        // like Flex/Flexboxes
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: <Widget>[
+          Row(
+            children: <Widget>[Text('Hello'), Text('World')],
+          ),
+          Container(
+            child: Text('Flutter Demo App'),
+          ),
+          Container(
+            margin: EdgeInsets.symmetric(horizontal: 200.0, vertical: 20.0),
+            child: TextButton(
+              onPressed: () => print('clicked genrate button'),
+              child: Text('Generate Memes'),
+              style: textButtonStyle,
+            ),
+          )
+        ],
+      ),
+      floatingActionButton: FloatingActionButton(
+        child: Icon(Icons.bolt), // Text("Click me")
+        onPressed: () => print("clicked"),
+        backgroundColor: Colors.blue[400],
+      ),
+    );
   }
 }
